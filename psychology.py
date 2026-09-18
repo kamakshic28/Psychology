553ced4e02dab26212e2f7794c9923b9adc5cc3c
Kamakshi Chauhan <98561301+kamakshic28@users.noreply.github.com>
2026-09-17 23:52:51 -0400

    Add files via upload

--- psychology.py
+++ psychology.py
@@ -92,3 +92,4 @@ print('Psychology of the oldest child')
 print('The oldest child learns to take responsibility faster than the second, even the silent responsibilities.')
 
 print((9-0))
+print(0*0)
